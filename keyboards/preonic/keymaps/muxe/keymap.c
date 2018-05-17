d1d800f1582ef22d6ddae626fced895965b662f4
Max-Richard Barz <max-richard.barz@siemens.com>
2018-05-17 09:58:09 +0200

    pgup and pgdown

--- keyboards/preonic/keymaps/muxe/keymap.c
+++ keyboards/preonic/keymaps/muxe/keymap.c
@@ -76,14 +76,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
   _______, _______, _______, _______, _______, _______, KC_ENT,  _______, KC_MNXT, KC_VOLD, KC_VOLU,  _______ \
 ),
 
 [_RAISE] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_GRV,  KC_1,    KC_2,    KC_LCBR, KC_RCBR, KC_RPRN, KC_6,    KC_MINS, KC_UP,   KC_EQL,  KC_0,    KC_F12,  \
+  KC_GRV,  KC_1,    KC_2,    KC_LCBR, KC_RCBR, KC_RPRN, KC_6,    KC_PGUP, KC_UP,   KC_PGDN, KC_0,    KC_F12,  \
   _______, KC_F1,   KC_F2,   KC_LPRN, KC_RPRN, KC_RBRC, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_LBRC, KC_RBRC, KC_F11,  KC_END,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, _______  \
